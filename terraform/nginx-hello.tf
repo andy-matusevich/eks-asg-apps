260a6042531803329b169f1ca7e91adcf6d72786
Andrei Matusevich <andy.matusevich@gmail.com>
2020-06-30 00:56:38 +0300

    test k8s deployment

--- terraform/nginx-hello.tf
+++ terraform/nginx-hello.tf
@@ -9,12 +9,38 @@ resource "kubernetes_namespace" "apps" {
   }
 }
 
-data "kubernetes_service" "nginx-hello" {
+resource "kubernetes_service" "nginx-hello" {
   metadata {
     name      = var.nginx-hello-name
     namespace = kubernetes_namespace.apps.metadata[0].name
     labels    = {
       app = var.nginx-hello-name
+    }
+  }
+  spec {
+    selector         = {
+      app = var.nginx-hello-name
+    }
+    session_affinity = "ClientIP"
+    port {
+      port        = 80
+      target_port = 80
+    }
+  }
+}
+
+resource "kubernetes_pod" "nginx-hello" {
+  metadata {
+    name   = var.nginx-hello-name
+    labels = {
+      app = var.nginx-hello-name
+    }
+  }
+
+  spec {
+    container {
+      image = "${data.terraform_remote_state.aws.outputs.ecr_registry_url}:${var.commit_sha1}"
+      name  = var.nginx-hello-name
     }
   }
 }
@@ -61,21 +87,6 @@ resource "kubernetes_deployment" "nginx-hello" {
               memory = "50Mi"
             }
           }
-
-          #          liveness_probe {
-          #            http_get {
-          #              path = "/"
-          #              port = 80
-          #
-          #              http_header {
-          #                name  = "X-Custom-Header"
-          #                value = "Awesome"
-          #              }
-          #            }
-
-          #            initial_delay_seconds = 3
-          #            period_seconds        = 3
-          #          }
         }
       }
     }
@@ -97,7 +108,7 @@ resource "kubernetes_ingress" "nginx-hello" {
             service_name = kubernetes_deployment.nginx-hello.metadata[0].name
             service_port = "80"
           }
-          path = "/nginx-hello"
+          path = "/nginx"
         }
       }
     }
