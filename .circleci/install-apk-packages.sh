ebf31c9b0345c76ce95fe828c6d4913f1582b500
Andrei Matusevich <andy.matusevich@gmail.com>
2020-06-29 23:25:59 +0300

    test k8s deployment

--- .circleci/install-apk-packages.sh
+++ .circleci/install-apk-packages.sh
@@ -1,10 +1,10 @@
 #!/bin/sh -leo pipefail
 
-apk add --no-cache --update g++ python3-dev libffi-dev openssl openssl-dev sudo curl
+apk add --no-cache --update --no-progress g++ python3-dev libffi-dev openssl openssl-dev sudo curl
 echo "Set disable_coredump false" >> /etc/sudo.conf
-apk add --no-cache python3 py3-pip
-pip3 install --upgrade setuptools
-pip3 install --upgrade awscli
+apk add --no-cache --no-progress python3 py3-pip
+pip3 install --upgrade setuptools --progress-bar off
+pip3 install --upgrade awscli --progress-bar off
 cd /usr/local/bin && \
 curl https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip \
     -o terraform_${TERRAFORM_VERSION}_linux_amd64.zip && \
