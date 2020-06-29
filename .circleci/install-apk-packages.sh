0ae8757e15b1b491e1e7f20475618e344f2aa044
Andrei Matusevich <andy.matusevich@gmail.com>
2020-06-29 18:04:05 +0300

    use docker image

--- .circleci/install-apk-packages.sh
+++ .circleci/install-apk-packages.sh
@@ -1,10 +1,11 @@
 #!/bin/sh -leo pipefail
 
-apk add --no-cache --update g++ python3-dev libffi-dev openssl openssl-dev sudo
+apk add --no-cache --update g++ python3-dev libffi-dev openssl openssl-dev sudo curl
 echo "Set disable_coredump false" >> /etc/sudo.conf
-apk add --no-cache curl python3 py3-pip setuptools
+apk add --no-cache python3 py3-pip
 apk add --no-cache docker
-
+pip3 install --upgrade setuptools
+pip3 install --upgrade awscli
 cd /usr/local/bin && \
 curl https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip \
     -o terraform_${TERRAFORM_VERSION}_linux_amd64.zip && \
