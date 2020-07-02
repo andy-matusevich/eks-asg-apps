e98e2c703f3a24fd580c41bc05411cc4b9bfb07c
Andrei Matusevich <andy.matusevich@gmail.com>
2020-07-02 13:15:27 +0300

    test helm release

--- .circleci/install-apk-packages.sh
+++ .circleci/install-apk-packages.sh
@@ -2,7 +2,7 @@
 
 apk add --no-cache --update --no-progress g++ python3-dev libffi-dev openssl openssl-dev sudo curl
 echo "Set disable_coredump false" >> /etc/sudo.conf
-apk add --no-cache --no-progress python3 py3-pip
+apk add --no-cache --no-progress python3 py3-pip bash
 pip3 install --upgrade setuptools --progress-bar off
 pip3 install --upgrade awscli --progress-bar off
 cd /usr/local/bin && \
